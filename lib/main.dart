ec46342541bedc043a30108a3ec62df290c16ad0
boring-km <kms0644804@naver.com>
2022-09-17 02:28:05 +0900

    [feat/ACP-39] flutter_background_service에서 File 변경 감지해서 서버 API 전송하는 시점 표시

--- lib/main.dart
+++ lib/main.dart
@@ -1,9 +1,11 @@
 import 'dart:async';
 import 'dart:io';
+import 'dart:ui';
 
 import 'package:ac_project_app/routes.dart';
 import 'package:ac_project_app/util/logger.dart';
 import 'package:flutter/material.dart';
+import 'package:flutter_background_service/flutter_background_service.dart';
 import 'package:flutter_dotenv/flutter_dotenv.dart';
 import 'package:get/route_manager.dart';
 import 'package:kakao_flutter_sdk_user/kakao_flutter_sdk_user.dart';
@@ -36,4 +38,40 @@ class MyApp extends StatelessWidget {
       getPages: Pages.pages,
     );
   }
+}
+
+Future<void> initBackgroundService() async {
+  final service = FlutterBackgroundService();
+
+  await service.configure(
+    androidConfiguration: AndroidConfiguration(
+      onStart: onStart,
+      isForegroundMode: true,
+    ),
+    iosConfiguration: IosConfiguration(
+      onForeground: onStart,
+      onBackground: onIosBackground,
+    ),
+  );
+
+  unawaited(service.startService());
+}
+
+
+bool onIosBackground(ServiceInstance service) {
+  WidgetsFlutterBinding.ensureInitialized();
+  // TODO 여기서 File 내용 바뀌는 것 확인해서 서버 업로드
+
+  return true;
+}
+
+@pragma('vm:entry-point')
+Future<void> onStart(ServiceInstance service) async {
+  DartPluginRegistrant.ensureInitialized();
+
+  service.on('stopService').listen((event) {
+    service.stopSelf();
+  });
+
+  // TODO 여기서 File 내용 바뀌는 것 확인해서 서버 업로드
 }
