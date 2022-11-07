42f54e4200d4a7c768939ee9da85b31c7d2cdde2
jason <stacey5165@gmail.com>
2022-11-07 19:20:50 +0900

    [ACP-120] db 연결

---
+++ ios/Extension/ShadowView.swift
@@ -0,0 +1,64 @@
+//
+//  ShadowView.swift
+//  Extension
+//
+//  Created by 유재선 on 2022/10/18.
+//
+
+import UIKit
+
+@IBDesignable
+class ShadowView: UIImageView {
+    //Shadow
+    
+    @IBInspectable var topPadding: CGFloat = 0.0
+    @IBInspectable var leftPadding: CGFloat = 0.0
+    @IBInspectable var bottomPadding: CGFloat = 0.0
+    @IBInspectable var rightPadding: CGFloat = 0.0
+    
+    convenience init(padding: UIEdgeInsets) {
+        self.init()
+        self.topPadding = padding.top
+        self.leftPadding = padding.left
+        self.bottomPadding = padding.bottom
+        self.rightPadding = padding.right
+    }
+    
+    override var intrinsicContentSize: CGSize {
+        var contentSize = super.intrinsicContentSize
+        contentSize.width += self.leftPadding + self.rightPadding
+        contentSize.height += self.topPadding + self.bottomPadding
+        return contentSize
+    }
+    
+    
+    @IBInspectable var shadowColor: UIColor = UIColor.black {
+        didSet {
+            self.updateView()
+        }
+    }
+    @IBInspectable var shadowOpacity: Float = 0.5 {
+        didSet {
+            self.updateView()
+        }
+    }
+    @IBInspectable var shadowOffset: CGSize = CGSize(width: 3, height: 3) {
+        didSet {
+            self.updateView()
+        }
+    }
+    @IBInspectable var shadowRadius: CGFloat = 15.0 {
+        didSet {
+            self.updateView()
+        }
+    }
+
+    //Apply params
+    func updateView() {
+        self.layer.shadowColor = self.shadowColor.cgColor
+        self.layer.shadowOpacity = self.shadowOpacity
+        self.layer.shadowOffset = self.shadowOffset
+        self.layer.shadowRadius = self.shadowRadius
+    }
+}
+	
--- ios/Extension/ShareViewController.swift
+++ ios/Extension/ShareViewController.swift
@@ -9,86 +9,128 @@ import UIKit
 import Social
 import MobileCoreServices
 import UniformTypeIdentifiers
-import SQLite3
+import OpenGraph
 
 class ShareViewController: UIViewController {
-  
-  
-  @IBOutlet weak var btnTest: UIButton!
-  
-  override func viewDidLoad() {
-    super.viewDidLoad()
     
-  }
-  
-  @IBAction func saveUrl(){
-    if let extensionItem = self.extensionContext?.inputItems[0] as? NSExtensionItem {
-      if let itemProviders = extensionItem.attachments{
-        for itemProvider in itemProviders{
-          if itemProvider.hasItemConformingToTypeIdentifier(UTType.url.identifier){
-            itemProvider.loadItem(forTypeIdentifier: UTType.url.identifier, options: nil){
-              (data, error) in
-              let text = (data as! NSURL).absoluteString!
-              
-              let sharedDefault = UserDefaults(suiteName: "group.com.mr.acProjectApp.Share")!
-              var savedData = sharedDefault.object(forKey: "shareDataList") as! [String]? ?? []
-              savedData.append(text)
-              sharedDefault.set(savedData, forKey: "shareDataList")
-              
-              self.hideExtensionWithCompletionHandler()
+    
+    @IBOutlet weak var checkImageView: UIImageView!
+    @IBOutlet weak var btnTest: UIButton!
+
+    @IBOutlet weak var linksTableView: UITableView!
+
+    var dataArray : [Folder] = []
+
+    let dbHelper = DBHelper.shared
+
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        self.linksTableView.delegate = self
+        self.linksTableView.dataSource = self
+
+    }
+
+
+    func saveUrl(){
+
+
+            if let extensionItem = self.extensionContext?.inputItems[0] as? NSExtensionItem {
+                if let itemProviders = extensionItem.attachments{
+                    for itemProvider in itemProviders{
+                        if itemProvider.hasItemConformingToTypeIdentifier(UTType.url.identifier){
+                            itemProvider.loadItem(forTypeIdentifier: UTType.url.identifier, options: nil){
+                                (data, error) in
+                                let text = (data as! NSURL).absoluteString!
+
+                                let sharedDefault = UserDefaults(suiteName: "group.com.mr.acProjectApp.Share")!
+                                var savedData = sharedDefault.object(forKey: "shareDataList") as! [String]? ?? []
+                                savedData.append(text)
+                                sharedDefault.set(savedData, forKey: "shareDataList")
+
+                                self.hideExtensionWithCompletionHandler()
+                            }
+                        } else if itemProvider.hasItemConformingToTypeIdentifier(UTType.plainText.identifier) {
+                            itemProvider.loadItem(forTypeIdentifier: UTType.plainText.identifier, options: nil) {
+                                (data, error) in
+                                let text = data as! String
+
+                                let sharedDefault = UserDefaults(suiteName: "group.com.mr.acProjectApp.Share")!
+                                var savedData = sharedDefault.object(forKey: "shareDataList") as! [String]? ?? []
+                                savedData.append(text)
+                                sharedDefault.set(savedData, forKey: "shareDataList")
+
+                                self.hideExtensionWithCompletionHandler()
+                            }
+                        }
+                    }
+                }
             }
-          } else if itemProvider.hasItemConformingToTypeIdentifier(UTType.plainText.identifier) {
-            itemProvider.loadItem(forTypeIdentifier: UTType.plainText.identifier, options: nil) {
-              (data, error) in
-              let text = data as! String
-              
-              let sharedDefault = UserDefaults(suiteName: "group.com.mr.acProjectApp.Share")!
-              var savedData = sharedDefault.object(forKey: "shareDataList") as! [String]? ?? []
-              savedData.append(text)
-              sharedDefault.set(savedData, forKey: "shareDataList")
-              
-              self.hideExtensionWithCompletionHandler()
+
+    }
+    
+    func hideExtensionWithCompletionHandler() {
+        UIView.animate(withDuration: 0.3, animations: {
+            self.navigationController?.view.transform = CGAffineTransform(translationX: 0, y:self.navigationController!.view.frame.size.height)
+        }, completion: { _ in
+            self.extensionContext?.completeRequest(returningItems: nil, completionHandler: nil)
+        })
+    }
+    
+    func sharedDirectoryURL() -> URL {
+        let fileManager = FileManager.default
+        return fileManager.containerURL(forSecurityApplicationGroupIdentifier: "group.com.mr.acProjectApp.Share")!
+    }
+
+}
+
+extension ShareViewController: UITableViewDelegate, UITableViewDataSource {
+    func tableView(_ linksTableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        self.dataArray.count
+    }
+
+    func tableView(_ linksTableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        guard let cell = self.linksTableView.dequeueReusableCell(withIdentifier: "tableViewCell") as? CustomTableViewCell
+        else { fatalError("can't get cell") }
+
+
+
+        do{
+            let url : String? = String(dataArray[indexPath.row].name)
+            if(url != nil){
+                cell.imageLinkView.image = UIImage(data : try Data(contentsOf: URL(string : String(dataArray[indexPath.row].name))!))
+
             }
-          }
+            cell.nameView.text = String(dataArray[indexPath.row].name)
+            if dataArray[indexPath.row].visible == 1 {
+
+            }
         }
-      }
+        catch {
+        }
+
+        return cell
     }
-  }
-  
-  func hideExtensionWithCompletionHandler() {
-    UIView.animate(withDuration: 0.3, animations: {
-      self.navigationController?.view.transform = CGAffineTransform(translationX: 0, y:self.navigationController!.view.frame.size.height)
-    }, completion: { _ in
-      self.extensionContext?.completeRequest(returningItems: nil, completionHandler: nil)
-    })
-  }
-  
-  func sharedDirectoryURL() -> URL {
-    let fileManager = FileManager.default
-    return fileManager.containerURL(forSecurityApplicationGroupIdentifier: "group.com.mr.acProjectApp.Share")!
-  }
-  
-  
-  func getDB() -> OpaquePointer? {
-    var db: OpaquePointer? = nil
-    
-    let dbPath = self.getShareDBUrl()
-    if sqlite3_open(dbPath, &db) == SQLITE_OK {
-      return db
+
+
+}
+
+
+class CustomTableViewCell: UITableViewCell {
+    @IBOutlet weak var imageLinkView: UIImageView!
+    @IBOutlet weak var nameView: UILabel!
+    @IBOutlet weak var visibleView: UIImageView!
+
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        // Initialization code
     }
-    
-    print("db open error")
-    return nil
-  }
-  
-  func getShareDBUrl() -> String? {
-    
-    let fileContainer = FileManager
-      .default
-      .containerURL(
-        forSecurityApplicationGroupIdentifier: "group.com.mr.acProjectApp.Share"
-      )
-    
-    return fileContainer?.appendingPathExtension("share.db").path
-  }
+
+    override func setSelected(_ selected: Bool, animated: Bool) {
+        super.setSelected(selected, animated: animated)
+
+        // Configure the view for the selected state
+    }
+
 }
