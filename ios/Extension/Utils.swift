ccf7df6757f50060baff116eb77a763b5650db0b
Kangmin <kms0644804@naver.com>
2022-12-10 23:16:32 +0900

    [fix] iOS 링크 기록된 시간을 UTC 시간으로 기록

--- ios/Extension/Utils.swift
+++ ios/Extension/Utils.swift
@@ -11,7 +11,7 @@ extension Date {
   static func ISOStringFromDate(date: Date) -> String {
     let dateFormatter = DateFormatter()
     dateFormatter.locale = Locale(identifier: "en_US_POSIX")
-    dateFormatter.timeZone = .autoupdatingCurrent
+    dateFormatter.timeZone = TimeZone(secondsFromGMT: 0)
     dateFormatter.dateFormat = "yyyy-MM-dd'T'HH:mm:ss"
     
     return dateFormatter.string(from: date).appending("Z")
@@ -20,7 +20,7 @@ extension Date {
   static func dateFromISOString(string: String) -> Date? {
     let dateFormatter = DateFormatter()
     dateFormatter.locale = Locale(identifier: "en_US_POSIX")
-    dateFormatter.timeZone = TimeZone.autoupdatingCurrent
+    dateFormatter.timeZone = TimeZone(secondsFromGMT: 0)
     dateFormatter.dateFormat = "yyyy-MM-dd'T'HH:mm:ssZ"
     
     return dateFormatter.date(from: string)
