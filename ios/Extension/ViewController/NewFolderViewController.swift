5c38eecf1b59c706c9c90c7d3bd668173d491429
Kangmin <kms0644804@naver.com>
2022-12-30 00:24:42 +0900

    [fix/#63] iOS: 공유패널 폴더명 중복일 때 error 표시하기

--- ios/Extension/ViewController/NewFolderViewController.swift
+++ ios/Extension/ViewController/NewFolderViewController.swift
@@ -21,6 +21,9 @@ class NewFolderViewController : UIViewController {
 
   @IBOutlet weak var firstSaveButton : UIButton!
   @IBOutlet weak var secondSaveButton: UIButton!
+  @IBOutlet weak var errorText: UILabel!
+  @IBOutlet weak var errorTextConstraint: NSLayoutConstraint!
+  
   
   var link: String?
   var imageLink: String?
@@ -53,6 +56,10 @@ class NewFolderViewController : UIViewController {
     setKeyboardObserver()
     
     self.newFolderNameField.returnKeyType = .done
+    
+    self.errorText.isHidden = true
+    self.errorTextConstraint.constant = 0
+    self.view.layoutIfNeeded()
   }
   
   override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
@@ -65,6 +72,14 @@ class NewFolderViewController : UIViewController {
       )
       viewController.saveType = SaveType.New
     }
+  }
+  
+  func showErrorText() {
+    self.newFolderNameField.text = ""
+    self.newFolderNameField.layer.sublayers![0].backgroundColor = UIColor.error.cgColor
+    self.errorText.isHidden = false
+    self.errorTextConstraint.constant = 16
+    self.view.layoutIfNeeded()
   }
   
   @IBAction func didEndOnExit(_ sender: Any) {
@@ -139,11 +154,13 @@ class NewFolderViewController : UIViewController {
     
     if self.link != nil, !(folderName?.isEmpty ?? true) {
       let result = dbHelper.insertData(name: folderName!, visible: visible, imageLink: imageLink)
-      UserDefaultsHelper.saveNewFolder(self.link!, folderName!, self.newFolderVisible)
-      
+      if result {
+        UserDefaultsHelper.saveNewFolder(self.link!, folderName!, self.newFolderVisible)
+        performSegue(withIdentifier: "saveSuccessSegue", sender: self)
+      } else {
+        showErrorText()
+      }
       NSLog("❇️ new folder save result: \(result)")
-      
-      performSegue(withIdentifier: "saveSuccessSegue", sender: self)
     }
   }
   
