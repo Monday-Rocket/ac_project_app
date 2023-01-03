7f79867d8009f798fa806e6dd59a203a59e30317
Kangmin <kms0644804@naver.com>
2023-01-04 08:49:32 +0900

    [version] 1.0.22 업데이트

--- lib/const/strings.dart
+++ lib/const/strings.dart
@@ -2,13 +2,13 @@
 const approveFirstLink = 'https://plip.kr/html/2e555eb1-0d11-48d2-9756-22e3bad18fb0.html';
 
 // 이용 약관
-const approveSecondLink = 'https://spot-harpymimus-46b.notion.site/LINKPOOL-f0c6fd16eecf4c8c96bb317421ecc174';
+const approveSecondLink = 'https://linkpoolpd.notion.site/LINKPOOL-6f28bbd4f0914ffdad1614a3655d06fa';
 
 // 개인정보 처리방침
-const personalInfoLink = 'https://spot-harpymimus-46b.notion.site/LINKPOOL-652aff7a651549e9bfd67cb16f8478de';
+const personalInfoLink = 'https://linkpoolpd.notion.site/LINKPOOL-71b2b45eda864d91882a6995bf20413f';
 
 // 도움말
-const helpLink = 'https://spot-harpymimus-46b.notion.site/5940b1c7e51f4a95996c4cb6f7356b50';
+const helpLink = 'https://linkpoolpd.notion.site/c6ea6729dfac4dbc89c95d294bca43f8';
 
 // API LINK
 const baseUrl = 'http://ac-project-api.ap-northeast-2.elasticbeanstalk.com';
