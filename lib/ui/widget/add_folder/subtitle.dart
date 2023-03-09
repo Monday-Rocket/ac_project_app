45a3a5fcf9d1a85777f096c2e37f815674c2bd36
boring-km <kms0644804@naver.com>
2023-03-09 15:42:41 +0900

    [LP-5-fix] 업로드 화면 반응형으로 수정

--- lib/ui/widget/add_folder/subtitle.dart
+++ lib/ui/widget/add_folder/subtitle.dart
@@ -1,14 +1,15 @@
 import 'package:ac_project_app/const/colors.dart';
 import 'package:flutter/material.dart';
+import 'package:flutter_screenutil/flutter_screenutil.dart';
 
 Text buildSubTitle(String text) {
   return Text(
     text,
-    style: const TextStyle(
+    style: TextStyle(
       fontWeight: FontWeight.w500,
-      fontSize: 16,
-      height: 19 / 16,
-      letterSpacing: -0.3,
+      fontSize: 16.sp,
+      height: (19 / 16).h,
+      letterSpacing: -0.3.w,
       color: grey800,
     ),
   );
