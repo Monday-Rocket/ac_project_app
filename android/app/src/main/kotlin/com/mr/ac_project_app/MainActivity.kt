4af94a8ad1522a5ea8d1b9a43436c72f0b6c8cff
boring-km <kms0644804@naver.com>
2024-11-19 13:41:49 +0900

    [fix] 1.0.69: AndroidManifest.xml 정리

--- android/app/src/main/kotlin/com/mr/ac_project_app/MainActivity.kt
+++ android/app/src/main/kotlin/com/mr/ac_project_app/MainActivity.kt
@@ -5,7 +5,6 @@ import io.flutter.embedding.android.FlutterFragmentActivity
 import io.flutter.embedding.engine.FlutterEngine
 import io.flutter.plugin.common.MethodChannel
 
-@Suppress("UNCHECKED_CAST")
 class MainActivity : FlutterFragmentActivity() {
     override fun configureFlutterEngine(flutterEngine: FlutterEngine) {
         super.configureFlutterEngine(flutterEngine)
